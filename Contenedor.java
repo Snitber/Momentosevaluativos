566ca419f2a16b35978921a332eaf1b90d148ff2
Snitber <jhonnier.ortega@campusucc.edu.co>
2026-03-10 03:19:00 +0000

    Contenedor

--- Contenedor.java
+++ Contenedor.java
@@ -1,31 +1,27 @@
 public class Contenedor {
-    // Atributos privados (Encapsulamiento)
     private String id;
     private String pais;
     private double peso;
 
-    // Constructor para inicializar el objeto
     public Contenedor(String id, String pais, double peso) {
         this.id = id;
         this.pais = pais;
         this.peso = peso;
     }
 
-    // Métodos Getter (Para obtener los datos)
     public String getPais() { return pais; }
     public double getPeso() { return peso; }
     public String getId() { return id; }
 
-    // Métodos Setter (Para modificar los datos)
-    public void setId(String id) {
-        this.id = id;
+    public void setPeso(double peso) {
+        this.peso = peso;
     }
 
     public void setPais(String pais) {
         this.pais = pais;
     }
 
-    public void setPeso(double peso) {
-        this.peso = peso;
+    public void setId(String id) {
+        this.id = id;
     }
-} // <--- Esta es la llave que cierra la clase. NO pongas más después de esta.
+}
