9c3bb9df80c36e23c0b33d0e5d35ed722483ede4
Snitber <jhonnier.ortega@campusucc.edu.co>
2026-03-10 03:22:24 +0000

    Gestor

--- GestorPuerto.java
+++ GestorPuerto.java
@@ -1,43 +1,18 @@
 public class GestorPuerto {
     private String[] listaBuques = new String[10];
-    private int cantidadActual = 0;
+    private int cantidad = 0;
     public ZonaAlmacenamiento zona = new ZonaAlmacenamiento();
 
     public void anclarBuque(String nombre) {
-        if (cantidadActual < 10) {
-            listaBuques[cantidadActual] = nombre;
-            cantidadActual++;
-            System.out.println("¡Buque '" + nombre + "' registrado en el muelle!");
+        if (cantidad < 10) {
+            listaBuques[cantidad++] = nombre;
+            System.out.println("Buque anclado.");
         } else {
-            System.out.println("ADVERTENCIA: No hay muelles disponibles (Máximo 10).");
+            System.out.println("ADVERTENCIA: No hay muelles.");
         }
-    }
-
-    public int disponibles() {
-        return 10 - cantidadActual;
-    }
-
-    public String[] getListaBuques() {
-        return listaBuques;
     }
 
     public void setListaBuques(String[] listaBuques) {
         this.listaBuques = listaBuques;
-    }
-
-    public int getCantidadActual() {
-        return cantidadActual;
-    }
-
-    public void setCantidadActual(int cantidadActual) {
-        this.cantidadActual = cantidadActual;
-    }
-
-    public ZonaAlmacenamiento getZona() {
-        return zona;
-    }
-
-    public void setZona(ZonaAlmacenamiento zona) {
-        this.zona = zona;
     }
 }
