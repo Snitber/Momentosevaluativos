6e77f7d899fcd42c8c9874a3194d930f84d30838
Snitber <jhonnier.ortega@campusucc.edu.co>
2026-03-10 02:30:07 +0000

    Contenedor

--- Contenedor.java
+++ Contenedor.java
@@ -1,15 +1,28 @@
 public class Contenedor {
-    private String id;
-    private String pais;
-    private double peso;
+        private String id;
+            private String pais;
+                private double peso;
 
-    public Contenedor(String id, String pais, double peso) {
+                    public Contenedor(String id, String pais, double peso) {
+                            this.id = id;
+                                    this.pais = pais;
+                                            this.peso = peso;
+                                                }
+
+                                                    public String getPais() { return pais; }
+                                                        public double getPeso() { return peso; }
+                                                            public String getId() { return id; }
+
+    public void setId(String id) {
         this.id = id;
-        this.pais = pais;
-        this.peso = peso;
     }
 
-    public String getPais() { return pais; }
-    public double getPeso() { return peso; }
-    public String getId() { return id; }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+                                                            }
 }
