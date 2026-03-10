1d5c1908a0a0545b57a1b9e0596566bd7a98b085
Snitber <jhonnier.ortega@campusucc.edu.co>
2026-03-10 02:34:46 +0000

    GestorPuertos

--- GestorPuerto.java
+++ GestorPuerto.java
@@ -1,27 +1,19 @@
 public class GestorPuerto {
-    private String[] muellesBuques = new String[10];
-    private int buquesAtendidos = 0;
-    public ZonaAlmacenamiento patio = new ZonaAlmacenamiento();
+    private String[] listaBuques = new String[10];
+    private int cantidadActual = 0;
+    public ZonaAlmacenamiento zona = new ZonaAlmacenamiento();
 
-    public void registrarEntradaBuque(String nombre) {
-        if (buquesAtendidos < 10) {
-            muellesBuques[buquesAtendidos++] = nombre;
-            System.out.println("Buque '" + nombre + "' anclado con éxito.");
+    public void anclarBuque(String nombre) {
+        if (cantidadActual < 10) {
+            listaBuques[cantidadActual] = nombre;
+            cantidadActual++;
+            System.out.println("¡Buque '" + nombre + "' registrado en el muelle!");
         } else {
-            // Esta es la advertencia que pide el documento técnico
-            System.out.println("ADVERTENCIA: No hay espacio en el array de buques.");
+            System.out.println("ADVERTENCIA: No hay muelles disponibles (Máximo 10).");
         }
     }
 
-    public int getEspaciosLibresBuques() {
-        return 10 - buquesAtendidos;
-    }
-
-    public String[] getMuellesBuques() {
-        return muellesBuques;
-    }
-
-    public void setMuellesBuques(String[] muellesBuques) {
-        this.muellesBuques = muellesBuques;
+    public int disponibles() {
+        return 10 - cantidadActual;
     }
 }
