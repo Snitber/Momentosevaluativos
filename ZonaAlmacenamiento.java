62a5c280eceb8d83bd0f830f0f8da2bd9bd6ce28
Snitber <jhonnier.ortega@campusucc.edu.co>
2026-03-10 03:19:51 +0000

    Zona

--- ZonaAlmacenamiento.java
+++ ZonaAlmacenamiento.java
@@ -2,19 +2,17 @@ public class ZonaAlmacenamiento {
     private Contenedor[][] matriz = new Contenedor[10][10];
 
     public void dibujarPatio() {
-        System.out.println("\n--- MAPA DE CONTENEDORES (F9=Suelo) ---");
+        System.out.println("\n--- MAPA DE PUESTOS ---");
         for (int i = 0; i < 10; i++) {
-            System.out.print("Fila " + i + " | ");
+            System.out.print("F" + i + " | ");
             for (int j = 0; j < 10; j++) {
-                System.out.print(matriz[i][j] == null ? "[  ] " : "[XX] ");
+                System.out.print(matriz[i][j] == null ? "[ ] " : "[X] ");
             }
             System.out.println();
         }
-        System.out.println("         0    1    2    3    4    5    6    7    8    9 (Cols)");
     }
 
     public boolean registrarEnColumna(int col, Contenedor c) {
-        // Lógica: busca desde la fila 9 hacia arriba para no dejar vacíos abajo
         for (int f = 9; f >= 0; f--) {
             if (matriz[f][col] == null) {
                 matriz[f][col] = c;
@@ -25,28 +23,23 @@ public class ZonaAlmacenamiento {
     }
 
     public double calcularPesoTotal() {
-        double suma = 0;
+        double total = 0;
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (matriz[i][j] != null) suma += matriz[i][j].getPeso();
+                if (matriz[i][j] != null) total += matriz[i][j].getPeso();
             }
         }
-        return suma;
+        return total;
     }
 
     public void reportePorPais() {
-        System.out.println("\n--- INVENTARIO POR ORIGEN ---");
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 if (matriz[i][j] != null) {
-                    System.out.println("ID: " + matriz[i][j].getId() + " | País: " + matriz[i][j].getPais() + " | Ubicación: ["+i+"]["+j+"]");
+                    System.out.println("ID: " + matriz[i][j].getId() + " - Pais: " + matriz[i][j].getPais());
                 }
             }
         }
-    }
-
-    public Contenedor[][] getMatriz() {
-        return matriz;
     }
 
     public void setMatriz(Contenedor[][] matriz) {
